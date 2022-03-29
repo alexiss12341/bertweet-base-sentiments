21be32f26a116db606483f12e10c8b03893e179d
Alexiss Asotigue <asdsewczrt@gmail.com>
2022-03-29 16:46:34 +0800

    dl_coourse

---
+++ dl_course/app.py
@@ -0,0 +1,12 @@
+import gradio as gr
+
+from transformers import pipeline
+
+pipe = pipeline(task="text-classification", 
+                 model="finiteautomata/bertweet-base-sentiment-analysis")
+
+
+gr.Interface.from_pipeline(pipe, 
+                           title="Bertweet Base Sentiment Analysis",
+                           description="Tweet Classification using POS, NEU, NEG labels",
+                           allow_flagging="never").launch(inbrowser=True).
